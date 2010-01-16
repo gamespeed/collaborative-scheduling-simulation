41da21f7f3163bf9d576c7231125e1bfbea02847
liyaohua.bupt <liyaohua.bupt@119b8756-fe5a-11de-ac2f-e974ed891282>
2010-01-16 06:28:49 +0000

    修改了一个小BUG

--- trunk/test_batch.pl
+++ trunk/test_batch.pl
@@ -1,18 +1,22 @@
-my $dag_num=0;
+my $dag_num=10;
 my $dag=0;
-my $mac_num=0;
+my $mac_num=10;
 my $mac=0;
 my $dag_file_name;
 my $mac_file_name;
 
-for($mac=1;$mac<=$mac_num;$mac_num++)
+for($mac=1;$mac<=$mac_num;$mac++)
 {
-	$mac_file_name = "Machine.in$mac.txt";
-	for($dag=1;$dag<=1000;$dag++)
+#	$mac_file_name = "Machine.in"+$mac+".txt";
+	for($dag=1;$dag<=$dag_num;$dag++)
 	{
-		$dag_file_name = "DAG.in$dag.txt";
-		system("echo $file_name");
-		system("echo $file_name >>result.txt");
+		$mac_file_name = "Machine.in".$mac.".txt";
+		$dag_file_name = "DAG.in".$dag.".txt";
+#		print $mac_file_name;
+#		system("echo $file_name");
+#		system("echo $file_name >>result.txt");
+		print "Processing Machine $mac  DAG $dag\n";
 		system("main_cpp_unprotected.exe $mac_file_name $dag_file_name >> result.txt");
 	}
 }
+print "end\n";

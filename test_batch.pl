c414cf012110f05f2267d758f8698a9b344caa4f
liyaohua.bupt <liyaohua.bupt@119b8756-fe5a-11de-ac2f-e974ed891282>
2010-01-20 03:35:05 +0000

    测试的Perl脚本添加了删除以前数据的功能。添加求数据均值的脚本。仿真程序改为每次只输出makespan

--- test_batch.pl
+++ test_batch.pl
@@ -1,3 +1,5 @@
+system("del result.txt");
+
 my $dag_num=10;
 my $dag=0;
 my $mac_num=10;
@@ -11,7 +13,7 @@ for($mac=1;$mac<=$mac_num;$mac++)
 	for($dag=1;$dag<=$dag_num;$dag++)
 	{
 		$mac_file_name = "Machine.in".$mac.".txt";
-		$dag_file_name = "DAG.in".$dag.".txt";
+		$dag_file_name = "DAG.dup.in".$dag.".txt";
 #		print $mac_file_name;
 #		system("echo $file_name");
 #		system("echo $file_name >>result.txt");
@@ -19,4 +21,4 @@ for($mac=1;$mac<=$mac_num;$mac++)
 		system("main_cpp_unprotected.exe $mac_file_name $dag_file_name >> result.txt");
 	}
 }
-print "end\n";
+

2347fbb72dee91824b8b38e68bf6c43a36326dfc
liyaohua.bupt <liyaohua.bupt@119b8756-fe5a-11de-ac2f-e974ed891282>
2010-03-08 07:46:54 +0000

    自动识别结果文件中处理器的数量

--- trunk/collect_data.pl
+++ trunk/collect_data.pl
@@ -3,6 +3,7 @@ my $record;
 my $total_cpu;
 my $total_gpu;
 my $test1,$test2;
+my $proc_num;
 while($record=<FILE>)
 {
 	$total = $total + $record;
@@ -13,18 +14,20 @@ print "\n";
 
 $total_cpu = 0;
 $total_gpu = 0;
+$proc_num = 0;
 open(FILE, "result_util.txt");
 while($record=<FILE>)
 {
 #下面这行正则是为了将每行中的CPU利用率和GPU利用率匹配出来
-	if($record =~ /([0-1]\.\d+)\s([0-1]\.?\d*)/)
+	if($record =~ /([0-1]\.?\d*)\s([0-1]\.?\d*)/)
 	{
+		$proc_num = $proc_num +1;
 		$total_cpu = $total_cpu + $1;
 		$total_gpu = $total_gpu + $2;
-		print $1;
-		print "\n";
-		print $2;
-		print "\n";
+#		print $1;
+#		print "\n";
+#		print $2;
+#		print "\n";	
 	}
 #	print $record;
 #	$total = $total + $record;
@@ -35,8 +38,8 @@ while($record=<FILE>)
 #	$record = <FILE>;
 #	$total1 = $total1 + $record;
 }
-print "CPU:".$total_cpu/200;
+print "CPU:".$total_cpu/$proc_num;
 print "\n";
-print "GPU:".$total_gpu/200;
+print "GPU:".$total_gpu/$proc_num;
 print "\n";
 system("pause");
